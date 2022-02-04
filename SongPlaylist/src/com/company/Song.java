e1dcd066a49abf0e83baffbd6ca2c7e745abd8f4
Paras  Rastogi <parasr@chimeratechnologies.com>
2022-02-04 12:48:18 +0530

    new files added for LinkedList Example

---
+++ SongPlaylist/src/com/company/Song.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Song {
+    private String Title;
+    private double Duration;
+
+    public Song(String title, double duration) {
+        Title = title;
+        Duration = duration;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    @Override
+    public String toString() {
+        return this.Title+" : "+this.Duration;
+    }
+}
