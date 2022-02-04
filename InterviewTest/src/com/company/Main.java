a0052d8420896573e156e1e5e7e6ea50212c652c
Paras  Rastogi <parasr@chimeratechnologies.com>
2022-02-04 12:54:51 +0530

    commit remaining files

--- InterviewTest/src/com/company/Main.java
+++ InterviewTest/src/com/company/Main.java
@@ -4,8 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Helo");
-        String s ="Paras";
-        System.out.println(s.length());
+        int number  = 24;
+        int duplicateNumber = 24;
+        int fact =1;
+        for(int i =1;i<number;i++){
+                if(duplicateNumber % i == 0){
+                    System.out.println(duplicateNumber+" check number");
+                    duplicateNumber = duplicateNumber/i;
+                }
+                if(duplicateNumber == 1){
+                    System.out.println(number+" is the factorial of "+i);
+                    break;
+                }
+        }
     }
 }
